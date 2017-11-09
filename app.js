e97c421fa005be89ab1e1265f89f0fd9a928a513
emurugu <elamuruguk@gmail.com>
2017-11-10 12:39:33 +1300

    Updated mysql ip

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.use(
 
     connection(mysql,{
 
-        host: '172.30.235.5', // host mysql service
+        //host: '172.30.235.5', // host mysql service
+        host: '172.30.54.150', // host mysql service
         user: 'dbuser',
         password : 'password',
         port : 3306, // port mysql service
